610c6d7dc8d1eae3ae25de9e418ef1cddd75607f
pmaqi <pmaqi@DESKTOP-QIJPPTG>
2024-01-18 14:18:51 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSC180Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! ---");
+        }
+    }
+}
