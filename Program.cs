e03382f38362f676586b9db9ad9399c95f61d371
pmaqi <pmaqi@DESKTOP-QIJPPTG>
2024-03-28 15:01:22 -0700

    time class demo

--- Program.cs
+++ Program.cs
@@ -1,35 +1,26 @@
-﻿using System;
-public class Book
+﻿// Program class with Main method
+using System;
+namespace CSC180
 {
-    public string title;
-    public string isbn;
-    public double price;
-    public Book()
+    public class Program
     {
-        title = "";
-        isbn = "";
-        price = 0.0;
-    }
-    public Book(string t, string i, double p)
-    {
-        title = t;
-        isbn = i;
-        price = p;
-    }
-    public static void Display(Book b)
-    { Console.WriteLine($"Title: {b.title}, ISBN: {b.isbn}, Price: {b.price}"); }
-}
+        public static void Main(string[] args)
+        {
+            Time t1 = new Time();
+            t1.Hour = 11;
+            t1.Minute = 8;
+            t1.Second = 3.14;
+            Console.WriteLine(t1);
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Book book1 = new Book("book 1", "121", 10.99);
-        Book book2 = new Book("book 2", "122", 12.99);
-        Book book3 = new Book("book 3", "123", 18.88);
-        Book book4 = new Book("book 4", "124", 5.99);
-        Book book5 = new Book("book 5", "125", 20.0);
-        //book5.Display();
-        Book.Display(book3);
+            Time t2 = new Time(11, 8, 3.14);
+            Console.WriteLine(t2);
+
+            // test various "WriteTime" methods
+            Time.WriteTime(t1);
+            Time.WriteTime2(t1);
+            t1.WriteTime3();
+            t1.WriteTime4(t1);
+            t2.WriteTime4(t1);
+        }
     }
 }
