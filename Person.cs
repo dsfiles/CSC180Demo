bf80756ed751417012ee39b455de0c885292d0a7
pmaqi <pmaqi@DESKTOP-QIJPPTG>
2024-04-02 15:40:02 -0700

    qqqq

--- Person.cs
+++ Person.cs
@@ -1,5 +1,5 @@
-﻿abstract class Person // Parent class
-{
+﻿abstract class Person
+{ // Parent class
     public string name;
     public int age;
     public Person(string name, int age)
@@ -8,17 +8,27 @@
         this.age = age;
     }
     public void DisplayNameAge()
-    { System.Console.WriteLine($"{name} is {age} years old."); }
+    { System.Console.WriteLine($"Name：{name}, Age: {age}"); }
 }
 class Teacher : Person
-{ // Child class 
+{ // Child class
     public string subject;
     public Teacher(string name, int age, string subject) : base(name, age)
     { this.subject = subject; }
+    public void DisplayTeacher()
+    {
+        base.DisplayNameAge();
+        System.Console.WriteLine($"Teaches {subject}\n");
+    }
 }
 class Student : Person
 { // Child class
     public double gpa;
     public Student(string name, int age, double gpa) : base(name, age)
     { this.gpa = gpa; }
+    public void DisplayStudent()
+    {
+        base.DisplayNameAge();
+        System.Console.WriteLine($"GPA: {this.gpa}");
+    }
 }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Person person = new Person("Allan", 21);
+        //Person person = new Person("Allan", 21);
         Teacher teacher = new Teacher("Tom", 55, "Computer Science");
         teacher.DisplayNameAge();
         Student student = new Student("Sara", 19, 3.5);
