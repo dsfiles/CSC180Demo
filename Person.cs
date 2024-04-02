fff7596ad6b5ded8a5da1557ee8045c8d38b57f0
pmaqi <pmaqi@DESKTOP-QIJPPTG>
2024-04-02 15:22:00 -0700

    aaa

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Person // parent class
+{
+    public string name;
+    public int age;
+    public void DisplayNameAge()
+    {
+        Console.WriteLine($"{name} is {age} years old.");
+    }
+}
+
+class Teacher : Person // Child class
+{
+    public string subject;
+}
+
+class Student : Person // Child class
+{
+    public double gpa;
+}
--- Program.cs
+++ Program.cs
@@ -1,40 +1,23 @@
-﻿// Another version using C# Properties
-using System;
+﻿using System;
 
-namespace CSC180Quiz03
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            // car1 does not have any information.
-            var car1 = new Vehicle();
-            var car2 = new Vehicle() { Vin = "1FM0122021", Make = "Ford", Model = "Mercury", Mileage = 0 };
-            car2.Drive(100);
-            Console.Write("Car #1: ");
-            car1.Display();
-            Console.Write("Car #2: ");
-            car2.Display();
-            car2.Drive(90);
-            Console.Write("After driving car #2 90 miles\nCar #2: ");
-            car2.Display();
-        }
-    }
+        Person person = new Person();
+        person.name = "Allan";
+        person.age = 21;
 
-    public class Vehicle
-    {
-        public string Vin { get; set; }
-        public string Make { get; set; }
-        public string Model { get; set; }
-        public int Mileage { get; set; }
+        Teacher teacher = new Teacher();
+        teacher.name = "Tom";
+        teacher.age = 55;
+        teacher.subject = "Computer Science";
+        teacher.DisplayNameAge();
 
-        public void Drive(int miles)
-        {
-            this.Mileage += miles;
-        }
-        public void Display()
-        {
-            Console.WriteLine($"Vin: {Vin}, make: {Make}, Model: {Model}, Mileage: {Mileage}");
-        }
+        Student student = new Student();
+        student.name = "Sara";
+        student.age = 19;
+        student.gpa = 3.5;
+        student.DisplayNameAge();
     }
 }
