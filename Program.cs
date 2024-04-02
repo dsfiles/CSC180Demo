d4a4ea589178dc49a1ab165e81d84dd2e8a23e45
pmaqi <pmaqi@DESKTOP-QIJPPTG>
2024-04-02 15:12:14 -0700

    sDAS

--- Program.cs
+++ Program.cs
@@ -1,86 +1,40 @@
-﻿using System;
+﻿// Another version using C# Properties
+using System;
 
-namespace Quiz03
-
+namespace CSC180Quiz03
 {
-    class Program
-
+    public class Program
     {
-
         static void Main(string[] args)
-
         {
-
-            Console.WriteLine("Car #1");
-
-            Car car = new Car("1234", "Ford", "Focus", 98000);
-
-            car.Drive(200);
-
-            car.Display();
-
-
-            Console.WriteLine("Car #2");
-
-            Car car2 = new Car("7777", "Honda", "Civic", 2300);
-
-            car2.Drive(1200);
-
+            // car1 does not have any information.
+            var car1 = new Vehicle();
+            var car2 = new Vehicle() { Vin = "1FM0122021", Make = "Ford", Model = "Mercury", Mileage = 0 };
+            car2.Drive(100);
+            Console.Write("Car #1: ");
+            car1.Display();
+            Console.Write("Car #2: ");
             car2.Display();
-
+            car2.Drive(90);
+            Console.Write("After driving car #2 90 miles\nCar #2: ");
+            car2.Display();
         }
-
     }
 
-
-
-    class Car
-
+    public class Vehicle
     {
-
-        string vin;
-
-        string make;
-
-        string model;
-
-        int mileage;
-
-
-
-        public Car() { }
-
-        public Car(string vin, string make, string model, int mileage)
-
-        {
-
-            this.vin = vin;
-
-            this.make = make;
-
-            this.model = model;
-
-            this.mileage = mileage;
-
-        }
-
-
+        public string Vin { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Mileage { get; set; }
 
         public void Drive(int miles)
-
         {
-
-            mileage += miles;
-
+            this.Mileage += miles;
         }
-
-
-
         public void Display()
         {
-            Console.WriteLine("   Vin#: {0}\n   Make: {1}\n   Model: {2}\n   Mileage: {3}\n", vin, make, model, mileage);
+            Console.WriteLine($"Vin: {Vin}, make: {Make}, Model: {Model}, Mileage: {Mileage}");
         }
-
     }
-
 }
