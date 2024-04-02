88959cd74b497ad70a4ab03ff218eb83975f3d57
pmaqi <pmaqi@DESKTOP-QIJPPTG>
2024-04-02 15:01:00 -0700

    4/2

--- Program.cs
+++ Program.cs
@@ -1,26 +1,48 @@
-﻿// Program class with Main method
-using System;
-namespace CSC180
+﻿using System;
+
+namespace CSC180Quiz03
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Time t1 = new Time();
-            t1.Hour = 11;
-            t1.Minute = 8;
-            t1.Second = 3.14;
-            Console.WriteLine(t1);
+            // car1 does not have any information.
+            var car1 = new Vehicle();
+            var car2 = new Vehicle("1FM0122021", "Ford", "Mercury", 0);
+            car2.Drive(100);
+            Console.Write("Car #1: ");
+            car1.Display();
+            Console.Write("Car #2: ");
+            car2.Display();
+            car2.Drive(90);
+            Console.Write("After driving car #2 90 miles\nCar #2: ");
+            car2.Display();
+        }
+    }
 
-            Time t2 = new Time(11, 8, 3.14);
-            Console.WriteLine(t2);
-
-            // test various "WriteTime" methods
-            Time.WriteTime(t1);
-            Time.WriteTime2(t1);
-            t1.WriteTime3();
-            t1.WriteTime4(t1);
-            t2.WriteTime4(t1);
+    public class Vehicle
+    {
+        private string vin, make, model;
+        private int mileage;
+        public Vehicle() // constructor #1
+        {
+            vin = "N/A"; make = "N/A"; model = "N/A";
+            mileage = 0;
+        }
+        public Vehicle(string vin, string make, string model, int mileage)
+        {  // constructor #2 with parameters
+            this.vin = vin;
+            this.make = make;
+            this.model = model;
+            this.mileage = mileage;
+        }
+        public void Drive(int miles)
+        {
+            mileage += miles;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"vin: {vin}, make: {make}, model: {model}, mileage: {mileage}");
         }
     }
 }
