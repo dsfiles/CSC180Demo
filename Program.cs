fc52235a3937e1295a2d357dd38c306d2dabc96e
pmaqi <pmaqi@DESKTOP-QIJPPTG>
2024-04-11 14:57:58 -0700

    aa

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
 ﻿using System;
-public class Book {
-    public string title, isbn;
-    public double price;
+public enum Season
+{ //names of enum members
+    Spring,
+    Summer,
+    Autumn,
+    Winter
 }
-public class Program {
-    public static void Main(string[] args) {
-        Book book1 = new Book();
-        book1.title = "C# Programming";
-        book1.isbn = "180056810X";
-        book1.price = 42.74;
-        Console.WriteLine($"title: {book1.title}, ISBN: {book1.isbn}, Price: {book1.price}");
-        Console.WriteLine("book price increased!");
-        increateBookPrice(book1, 10);
-        Console.WriteLine($"title: {book1.title}, ISBN: {book1.isbn}, Price: {book1.price}");
-    }
-    public static void increateBookPrice(Book book, double price) {
-        book.price += price;
+public class EnumConversionExample
+{
+    public static void Main()
+    {
+        Season a = Season.Autumn;
+        Console.WriteLine($"Integral value of {a} is {(int)a}");
+        var b = (Season)1;
+        Console.WriteLine(b);  // output: Summer
+        var c = (Season)4;
+        Console.WriteLine(c);  // output: 4
     }
 }
