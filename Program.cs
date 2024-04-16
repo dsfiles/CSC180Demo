746447afcc15f1e12f87e6149aace078136323da
pmaqi <pmaqi@DESKTOP-QIJPPTG>
2024-04-16 14:52:52 -0700

    4/16

--- Program.cs
+++ Program.cs
@@ -1,21 +1,14 @@
-﻿// https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/tutorials/arrays-and-collections?WT.mc_id=Educationalcsharp-c9-scottha
-using System;
-using System.Collections.Generic;
+﻿using System;
 public class Program
 {
-    static void Main(string[] args)
+    unsafe static void Main(string[] args)
     {
-        var fibonacciNumbers = new List<int> { 1, 1 };
-        while (fibonacciNumbers.Count < 10)
-        {
-            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
-            fibonacciNumbers.Add(previous + previous2);
-        }
-
-        foreach (var number in fibonacciNumbers)
-        {
-            Console.WriteLine(number);
-        }
+        int i = 1;
+        int* p = &i;
+        Console.WriteLine((int)p);
+        Console.WriteLine(*p);
+        p++; // this is really unsafe
+        Console.WriteLine((int)p);
+        Console.WriteLine(*p); // oops
     }
 }
