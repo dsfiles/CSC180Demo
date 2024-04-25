8bc35ef5a8a0297df575c29add5051afb866cdb2
pmaqi <pmaqi@DESKTOP-QIJPPTG>
2024-04-25 14:57:01 -0700

    UPDATE

--- Program.cs
+++ Program.cs
@@ -4,8 +4,14 @@ public class Example
 {
     public static void Main()
     {
+        char c = 'a';
+        Console.WriteLine((int)c);
+        Console.WriteLine(c);
+        Console.WriteLine(c + 2);
+        Console.WriteLine('c' + 2);
+        Console.WriteLine((char)c + 2);
+        Console.WriteLine((char)(c+2));
 
-        Console.WriteLine("today is 4/25");
     }
   
 } // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.linkedlist-1?view=net-8.0
