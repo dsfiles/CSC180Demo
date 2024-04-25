072afd2e42b876bce358af94220716b53a21552c
pmaqi <pmaqi@DESKTOP-QIJPPTG>
2024-04-25 14:34:20 -0700

    4/25

--- Program.cs
+++ Program.cs
@@ -4,37 +4,8 @@ public class Example
 {
     public static void Main()
     {
-        // Create the link list.
-        string[] words =
-            { "the", "fox", "jumps", "over", "the", "dog" };
-        LinkedList<string> sentence = new LinkedList<string>(words);
-        Display(sentence, "The linked list values:");
-        Console.WriteLine("sentence.Contains(\"jumps\") = {0}", sentence.Contains("jumps"));
-        // Add the word 'today' to the beginning of the linked list.
-        sentence.AddFirst("today");
-        Display(sentence, "Test 1: Add 'today' to beginning of the list:");
-        // Move the first node to be the last node.
-        LinkedListNode<string> mark1 = sentence.First;
-        sentence.RemoveFirst();
-        sentence.AddLast(mark1);
-        Display(sentence, "Test 2: Move first node to be last node:");
-        // Change the last node to 'yesterday'.
-        sentence.RemoveLast();
-        sentence.AddLast("yesterday");
-        Display(sentence, "Test 3: Change the last node to 'yesterday':");
-        // Move the last node to be the first node.
-        mark1 = sentence.Last;
-        sentence.RemoveLast();
-        sentence.AddFirst(mark1);
-        Display(sentence, "Test 4: Move last node to be first node:");
+
+        Console.WriteLine("today is 4/25");
     }
-    private static void Display(LinkedList<string> words, string test)
-    {
-        Console.WriteLine(test);
-        foreach (string word in words)
-        {
-            Console.Write(word + " ");
-        }
-        Console.Write("\n\n");
-    }
+  
 } // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.linkedlist-1?view=net-8.0
