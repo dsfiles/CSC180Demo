5d6c9021fc728364cf194f8beaf9722dcabf7df2
GLi <guangyanli5@gmail.com>
2024-09-06 12:50:32 -0700

    Time project

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
-﻿namespace MyFirstProject
+﻿namespace Time
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("  ^  "); 
-            Console.WriteLine(" ^^^ "); 
-            Console.WriteLine("^^^^^"); 
-            //Console.ReadLine();
+            int hour, minute, second;
+            hour = 13; minute = 48; second = 55;
+            int intSecondSinceMidnight = hour*60*60 + minute*60 + second;
+            Console.WriteLine(intSecondSinceMidnight);
         }
     }
 }
