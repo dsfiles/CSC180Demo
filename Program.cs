9915b43bf3822af2618946c77c2ba68fda704e64
GLi <guangyanli5@gmail.com>
2024-09-06 12:32:41 -0700

    9/6 c# by Matt

--- Program.cs
+++ Program.cs
@@ -1,47 +1,14 @@
-﻿/* Kyle Stackpole
- * Week 12 homework
- */
-
-namespace Week_10_homework
+﻿namespace MyFirstProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string input = "Abc yz---";
-            int shiftAmount = 13;
-
-            string encoded = CaptainCrunch(input, shiftAmount);
-            Console.WriteLine("Encoded: " + encoded);
-
-            string decoded = CaptainCrunch(encoded, -shiftAmount);
-            Console.WriteLine("Decoded: " + decoded);
-        }
-
-        static string CaptainCrunch(string input, int shiftAmount)
-        {
-            char[] result = new char[input.Length];
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                char currentChar = input[i];
-
-                if (char.IsLetter(currentChar))
-                {
-                    bool isUpper = char.IsUpper(currentChar);
-                    char baseChar = isUpper ? 'A' : 'a';
-
-                    int shiftedChar = (currentChar - baseChar + shiftAmount) % 26;
-                    if (shiftedChar < 0) shiftedChar += 26;
-
-                    result[i] = (char)(shiftedChar + baseChar);
-                }
-                else
-                {
-                    result[i] = currentChar;
-                }
-            }
-            return new string(result);
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My Name is Matthew Camacho"); Console.ReadLine();
+            Console.WriteLine("  ^  "); Console.ReadLine();
+            Console.WriteLine(" ^^^ "); Console.ReadLine();
+            Console.WriteLine("^^^^^"); Console.ReadLine();
         }
     }
 }
